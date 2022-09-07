c4a7e7427da4a9853a5fccc529aeca3a3ea06ae2
nordl1ng <107497909+nordl1ng@users.noreply.github.com>
2022-09-07 10:47:28 +0500

    Перемешивание Random

--- task_5.py
+++ task_5.py
@@ -1,9 +1,14 @@
-# Реализуйте алгоритм перемешивания списка.
 import random
-from time import time
 n = int(input("Введите число N: "))
 list = []
 for i in range(0,n):
-    list.append(int(random.randint(n*-1, n)))
+    list.append(int(random.randint(-10, 10)))
+print(list)
+temp=0
+for i in range(0,n-1):
+    temp=list[i]
+    v=random.randint(0,n-1)
+    list[i]=list[v]
+    list[v]=temp
 print(list)
 
